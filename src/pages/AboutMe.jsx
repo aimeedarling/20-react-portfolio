56ed8d0d0fc4716279c7cba35832bf77d72e6647
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-08-27 22:18:23 -0700

    added react bootstrap with 6 card titles to portfolio section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,11 +1,11 @@
 export default function AboutMe() {
     return (
-        <div id="about-me-container">
-            {/* <img id="background-img" src="/src/assets/images/bence-biczo-AOFIIhX1njQ-unsplash.jpg" alt="photo of a pink house from unsplash" /> */}
-            <div className="content-block">
-                <h2>Hi, I'm Aimee!</h2>
-                <p></p>
-            </div>
+        <div id="about-me">
+            <h2>Hi, I'm Aimee!</h2>
+            <img id="selfie" src="src/assets/images/FXBR9229.jpeg" alt="avatar" />
+            <p className="bio-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus? </p>
         </div>
     )
 }
