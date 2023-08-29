76a403a1022149cc80230835b1f5671a92d3f8c5
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-08-28 18:35:32 -0700

    added screenshots

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,11 +1,11 @@
 export default function AboutMe() {
     return (
         <div id="about-me">
-            <h2>Hi, I'm Aimee!</h2>
-            <img id="selfie" src="src/assets/images/FXBR9229.jpeg" alt="avatar" />
-            <p className="bio-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus? </p>
+            <div className="text-zone">
+                <h2>Hi, I'm Aimee!</h2>
+                <img id="selfie" src="src/assets/images/FXBR9229.jpeg" alt="avatar" />
+                <p className="bio-text">Full stack Developer / MERN / </p>
+            </div>
         </div>
     )
 }
